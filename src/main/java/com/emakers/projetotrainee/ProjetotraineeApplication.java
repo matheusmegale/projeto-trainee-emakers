406ff130902e736ad8032682dba09fda950a5635
Matheus Megale <matheusmegale5@gmail.com>
2024-12-11 15:32:53 -0300

    Dependencias

---
+++ src/main/java/com/emakers/projetotrainee/ProjetotraineeApplication.java
@@ -0,0 +1,13 @@
+package com.emakers.projetotrainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotraineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotraineeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emakers/projetotrainee/ProjetotraineeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emakers.projetotrainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotraineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
